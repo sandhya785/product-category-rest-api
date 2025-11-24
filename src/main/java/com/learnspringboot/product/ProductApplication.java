d599d0fd44a0970f0a957f391a52f5d85072dbd0
sandhya785 <sandhyasara88@gmail.com>
2025-11-24 16:56:05 +0530

    Add complete project updates: Swagger documentation, JWT, validation, pagination, sorting and unit tests

--- src/main/java/com/learnspringboot/product/ProductApplication.java
+++ src/main/java/com/learnspringboot/product/ProductApplication.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
